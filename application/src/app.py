2f4ed9ee0fad0b741688d8e8021000ecc5fd7a59
abunuwas <joseharoperalta@gmail.com>
2016-09-01 17:55:52 +0100

    Start implementing Flask app

--- application/src/app.py
+++ application/src/app.py
@@ -1,31 +1,19 @@
 from flask import Flask
-from redis import Redis
+from flask import render_template, abort, redirect, url_for, request
+
 from os.path import expanduser 
 
 app = Flask(__name__)
-redis = Redis(host="redis")
 
 @app.route("/")
 def hello():
-    visits = redis.incr('counter')
-    html = "Hellow world! This is a new version :D :D :D :D <h1>This is definitely working!!!!!</h1><h2>Visits:</h2> <h2>{visits}</h2>"
-    return html.format(visits=visits)
+    return render_template('hello.html')
 
-@app.route("/new")
-def new():
-    return "This page is brand new!!!!!!!!!!!!!!!!"+html_files
-
-@app.route("/good")
-def good():
-    return "This is fucking good"
-
-@app.route("/see")
-def see():
-    return "Let's see if this works..."
-
-@app.route("/check")
-def check():
-    return "It works! It works!!"
+@app.route("/sendMetrics")
+def log_metric():
+	metric = request.args.get('metric_name')
+	value = request.args.get('metric_value')
+	return '{} = {}'.format(metric, value)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=6060)
